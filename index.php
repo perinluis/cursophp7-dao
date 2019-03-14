ef494b67bc641ee5af773ed0db59b94552b364ba
Luis Henrique Perin <luis.perin@hotmail.com>
2019-03-13 21:34:43 -0300

    Fazendo listas

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 require_once("Config.php");
 
+//Select sem a Classe Usuario
 /*
 $sql = new Sql;
 
@@ -9,11 +10,35 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios); 
 */
 
-
+//Carrega apenas um Usuario
+/*
 $root = new Usuario();
 
 $root->loadById(2);
 
 echo $root;
+*/
+
+//Carrega uma Lista de Usuarios
+/*
+$lista = Usuario::getList();
+
+echo json_encode($lista); 
+*/
+
+//Carrega uma lista buscada pelo decriçao do Usuario
+/*
+$search = Usuario::search("i");
+
+echo json_encode($search);
+*/
+
+//Carrega a lista de usuario com autenticação
+
+$usuario = new Usuario();
+
+$usuario->login("Luis", "123456");
+
+echo $usuario;
 
  ?>
